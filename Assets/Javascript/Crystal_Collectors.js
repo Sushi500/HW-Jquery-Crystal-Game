8f82731d7773e1347d3be9c005da8b1e150445b2
Susanta Mozumder <sushanta.mozumder@gmail.com>
2018-06-14 23:29:24 -0400

    more_css_and_html

--- Assets/Javascript/Crystal_Collectors.js
+++ Assets/Javascript/Crystal_Collectors.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
     var random = Math.floor(Math.random()*102+19);
-    
     $("#numberToGet").text(random);
+    
     
     var num1 = Math.floor(Math.random()*12+1);
     var num2 = Math.floor(Math.random()*12+1);
@@ -13,6 +13,7 @@ $(document).ready(function() {
     var wins = 0;
     var losses = 0;
     
+
     $("#numberWins").text(wins);
     $("#numberLosses").text(losses);
     
@@ -28,31 +29,53 @@ $(document).ready(function() {
         $("#score").text(userTotal);
     }
     
-    function winner() {
-        alert("You Won!!");
-        wins++;
-        $("#numberWins").text(wins);
-        reset();
-    }
     
-    function loser() {
-        alert("You Lose!!");
-        losses++;
-        $("#numberLosses").text(losses);
-        reset();
-    }
+    
+    
+    
     
     $("#image1").on("click", function() {
         userTotal = userTotal + num1;
         console.log("New userTotal " + userTotal);
         $("#score").text(userTotal);
-    
+    });
+
+    $("#image2").on("click", function() {
+        userTotal = userTotal + num2;
+        console.log("New userTotal " + userTotal);
+        $("#score").text(userTotal);
+    });
+
+    $("#image3").on("click", function() {
+        userTotal = userTotal + num3;
+        console.log("New userTotal " + userTotal);
+        $("#score").text(userTotal);
+    });
+
+    $("#image4").on("click", function() {
+        userTotal = userTotal + num4;
+        console.log("New userTotal " + userTotal);
+        $("#score").text(userTotal);
+    });         
+
         if (userTotal === random) {
             winner()
+        function winner() {
+        alert("You Won!!");
+        wins++;
+        $("#numberWins").text(wins);
+        reset();
+    }
         }
 
         else if (userTotal > random){
             loser()
+            function loser() {
+        alert("You Lose!!");
+        losses++;
+        $("#numberLosses").text(losses);
+        reset();
+    }
         };
     
-    })})
+    })
